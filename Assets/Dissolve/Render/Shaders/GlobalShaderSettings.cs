2e4f5e47b28d39f111629bceb1e29c0b8d0e24be
DreamFairy <500290@qq.com>
2022-11-06 23:22:10 +0800

    add auto dissolve

--- Assets/Dissolve/Render/Shaders/GlobalShaderSettings.cs
+++ Assets/Dissolve/Render/Shaders/GlobalShaderSettings.cs
@@ -7,6 +7,10 @@ public class GlobalShaderSettings : MonoBehaviour
     public Texture2D DissolveTex;
     public Texture2D RampTex;
     public Transform CenterDissolvePos;
+    public bool AutoDissolve = false;
+    public float DissolveSpeed = 1.0f;
+    public float CurrentDissolve = 0.0f;
+    [Range(0,150)]
     public float DissolveRadius = 0;
 
     [Range(-1,1)]
@@ -40,11 +44,21 @@ public class GlobalShaderSettings : MonoBehaviour
         Shader.SetGlobalTexture("_DissolveTex", DissolveTex);
         Shader.SetGlobalFloat("_Clip", ClipValue);
         Shader.SetGlobalVector("_DissolveCenter", Vector4.zero);
+
+        CurrentDissolve = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(AutoDissolve)
+        {
+            CurrentDissolve += DissolveSpeed;
+
+            if (CenterDissolvePos)
+            {
+                Shader.SetGlobalVector("_DissolveCenter", new Vector4(CenterDissolvePos.position.x, CenterDissolvePos.position.y, CenterDissolvePos.position.z, CurrentDissolve));
+            }
+        }
     }
 }
